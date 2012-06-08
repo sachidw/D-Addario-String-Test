bb1f33f6c610e1de673369afcfe9990ee0a0d887
mattfors <mfors1@binghamton.edu>
2012-06-08 19:11:40 -0400

    Alias update

--- loadalias.sh
+++ loadalias.sh
@@ -6,3 +6,4 @@ alias reset="./bin/reset"			#reset [string]
 						#reset [] - Resets all strings
 alias readfplot="bash ./scripts/ftest.sh"	#readfplot [string]
 alias tension="./bin/tension"
+alias btension="./bin/bringtension"
